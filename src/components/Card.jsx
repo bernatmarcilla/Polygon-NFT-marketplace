a176a0160da9f47c1b1437f075455fd7400e7350
ikcoin <ikcoin.kb@protonmail.com>
2021-09-30 18:06:43 +0200

    Card Style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
 
+const style = {
+    //backgroundColor: 'grey'
+};
+
 export default function Card(props) {
     return (
         <MDBCard style={{ maxWidth: '42rem', backgroundColor: '#222222' }}>
@@ -9,8 +13,12 @@ export default function Card(props) {
                 <MDBCardTitle>{props.name}</MDBCardTitle>
                 <MDBCardText>
                     {props.description}
-                    Owner: {props.owner}
                 </MDBCardText>
+                <hr />
+                <div style={style}>
+                    Owner: 0xB85Ec4586A4f7e16e6624A3fa148db2398753a79 {props.owner}
+                    <br />
+                </div>
                 <br />
                 <MDBBtn href='#'>Buy NFT</MDBBtn>
             </MDBCardBody>
