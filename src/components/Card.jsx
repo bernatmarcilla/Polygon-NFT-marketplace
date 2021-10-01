4eb748424ff43996496fe401ec7596e4e93162f9
ikcoin <ikcoin.kb@protonmail.com>
2021-10-01 18:51:58 +0200

    transferFrom function

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,12 +16,11 @@ function trasferNFT(abi, address, tokenId) {
     const tokenContract = new Web3EthContract(abi, address);
     tokenContract.setProvider('https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/polygon/mumbai');
 
-    console.log(account.account);
-    /*
-    tokenContract.methods.transfer().send({
-        from: 
+    tokenContract.methods.transferFrom().send({
+        from: "0xB85Ec4586A4f7e16e6624A3fa148db2398753a79",
+        to: account.account,
+        tokenId: tokenId
     })
-    */
 }
 
 function getNFTOwner(abi, address) {
