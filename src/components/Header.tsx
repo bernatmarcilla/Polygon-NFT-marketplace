205ee4c60ea695f45fa7b485cb8e775bee26f33c
ikcoin <ikcoin.kb@protonmail.com>
2021-10-07 17:26:03 +0200

    Link href

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable no-nested-ternary */
 import { useWeb3React } from "@web3-react/core";
-import { string } from "hardhat/internal/core/params/argumentTypes";
+import Link from 'next/link'
 
 import { Account } from "./Account";
 import { Balance } from "./Balance";
 import { ChainId } from "./ChainId";
+
+const style = {
+  backgroundColor: '#7530ff'
+};
 
 export function Header() {
   const { active, error, chainId } = useWeb3React();
@@ -15,7 +19,13 @@ export function Header() {
         <span className="text-lg font-bold">{chainId == 80001 ? "🟢" : error ? "🔴" : "🟠"}</span>
         <h1 className="btn btn-ghost btn-sm rounded-btn" >{chainId == 80001 ? "Polygon Network" : "No Polygon Network"}</h1>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <h1>Test</h1>
+        <Link href="/">
+          <h1 style={style} className="btn btn-ghost btn-sm rounded-btn">Marketplace</h1>
+        </Link>
+        &nbsp;&nbsp;
+        <Link href="/minting">
+          <h1 style={style} className="btn btn-ghost btn-sm rounded-btn">Mint NFT</h1>
+        </Link>
       </div>
       <div className="flex-none hidden px-2 mx-2 lg:flex">
         <div className="flex items-stretch">
