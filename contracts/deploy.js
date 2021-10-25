ac26520e0af81f0d36c5f8d4c6183c413096f9fb
ikcoin <ikcoin.kb@protonmail.com>
2021-10-25 18:09:22 +0200

    Ethers js mumbai provider

--- contracts/deploy.js
+++ contracts/deploy.js
@@ -1,16 +1,20 @@
 /*
-async function main() {
-  const MyNFT = await ethers.getContractFactory("MyNFT")
+const ethers = require('ethers');
 
-  // Start deployment, returning a promise that resolves to a contract object
-  const myNFT = await MyNFT.deploy()
-  console.log("Contract deployed to address:", myNFT.address)
+const provider = new ethers.providers.JsonRpcProvider("https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/polygon/mumbai");
+
+async function main() {
+    const MyNFT = await ethers.getContractFactory("MyNFT")
+
+    // Start deployment, returning a promise that resolves to a contract object
+    const myNFT = await MyNFT.deploy()
+    console.log("Contract deployed to address:", myNFT.address)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
 */
