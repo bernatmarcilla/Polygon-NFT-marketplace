15e5c66e45fe65c4cf7bfbee66a7bc42498b5aaa
ikcoin <ikcoin.kb@protonmail.com>
2021-12-23 17:13:37 +0100

    ipfs metadata script

---
+++ contracts/create_metadata.js
@@ -0,0 +1,9 @@
+/*
+import * as IPFS from 'ipfs-core'
+
+const ipfs = await IPFS.create()
+const { cid } = await ipfs.add('Hello world')
+console.info(cid)
+// QmXXY5ZxbtuYj6DnfApLiGstzPN7fvSyigrRee3hDWPCaf
+
+*/
